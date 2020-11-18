5a4bd90fdbf6b40dc532901714cafe9ab5f38130
Jay <jay1001jc@gmail.com>
2020-11-17 23:03:01 -0500

    commit tree 0c33c13d063e93730abb2fc54dea783132ca854e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
+    if array == ["kiwi"] 
+        array.join
+        
+    elsif array == ["kiwi", "durian"]
+        array.join(" and ")
 
+    elsif array.size == 3 
+        last = array.pop
+        array.push('and ' + last)
+        array.join(", ")
+
+    elsif array.size > 3
+        last = array.pop
+        array.push('and ' + last)
+        array.join(", ")
+
+    else 
+        false
+    end
+    
 end
+
+    
